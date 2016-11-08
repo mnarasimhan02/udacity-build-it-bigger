d9ef30851fc68bf0662aa6697b6c9de32acb9d84
mnarasimhan <maheshya@gmail.com>
2016-11-08 16:09:55 -0500

    corrected test errors

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/CloudModuleTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/CloudModuleTest.java
@@ -4,14 +4,20 @@ import android.test.AndroidTestCase;
 import android.util.Log;
 
 public class CloudModuleTest extends AndroidTestCase {
+    private static final String LOG_TAG = "CloudModuleTest";
+
+
+    @SuppressWarnings("unchecked")
 
     public void runCloudModuleTest() {
         String joke = null;
+        Log.v("NonEmptyStringTest", "Running NonEmptyStringTest test");
         JokesAsyncTask jokesAsyncTask = new JokesAsyncTask(getContext(), null);
         jokesAsyncTask.execute();
         try {
             joke = jokesAsyncTask.get();
-            Log.d("CloudModuleTest", "Retrieved a non-empty string successfully: " + joke);
+            Log.d(LOG_TAG, "Retrieved a non-empty string successfully: " + joke);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
